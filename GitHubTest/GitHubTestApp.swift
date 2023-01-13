6bd9ffb8be66c5191fc1cadaf5a32803ce27e8ef
Robert Martens <robertmartens@mac.com>
2023-01-13 13:15:16 +0900

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Robert Martens on 2023/01/13.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
